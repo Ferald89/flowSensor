0e9c1a4c0aefd13590dc252bd92ce82e9f5e3a39
Fernando Aldair <ferpool_ald@hotmail.com>
2020-05-12 16:28:23 -0500

    add labels on class MainApplicaton

---
+++ flowsensor/main.py
@@ -0,0 +1,16 @@
+""" Main Screen Program"""
+
+# local
+from guiscreen import MainApplication
+
+def main():
+  """Es el control de la pantalla y la configuración y arranque de la misma"""
+
+  #Inizialisamos el tk
+  root = MainApplication()
+  # root.label1['text']="15.0 Mpa"
+  root.mainloop()
+
+if __name__ == "__main__":
+  """Empieza a ejecutar la primera linea de codigo"""
+  main()
