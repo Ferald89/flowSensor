6e3221a12b98b12eb8bb25000b319857df3a52d2
armandocalvo <armando95.cp@gmail.com>
2020-05-14 15:40:43 -0500

    Lectura y muestreo OK

--- flowsensor/main.py
+++ flowsensor/main.py
@@ -34,7 +34,7 @@ def main():
       root.label3['text']="{:^} MPa".format(sensorFlow3.pressure)
       root.label4['text']="{:^} MPa".format(sensorFlow4.pressure)
 
-      root.mainloop()
+      #root.mainloop()
       root.update_idletasks()
       root.update()
 
