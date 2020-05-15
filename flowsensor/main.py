459ba0d79df0bb1c10afbe2a24e793b76a1cec39
Fernando Aldair <ferpool_ald@hotmail.com>
2020-05-14 22:42:57 -0500

    resolving conflicts

--- flowsensor/main.py
+++ flowsensor/main.py
@@ -2,7 +2,7 @@
 
 # local
 from guiscreen import MainApplication
-# from flowsensor import FlowSensor
+from flowsensor import FlowSensor
 
 # utilities
 import time
@@ -11,10 +11,10 @@ def main():
   """Es el control de la pantalla y la configuración y arranque de la misma"""
   #Instanciamos FlowSensor
 
-  # sensorFlow1 = FlowSensor(0)
-  # sensorFlow2 = FlowSensor(1)
-  # sensorFlow3 = FlowSensor(2)
-  # sensorFlow4 = FlowSensor(3)
+  sensorFlow1 = FlowSensor(0)
+  sensorFlow2 = FlowSensor(1)
+  sensorFlow3 = FlowSensor(2)
+  sensorFlow4 = FlowSensor(3)
 
   #Inicializamos el tk
   root = MainApplication()
@@ -24,15 +24,15 @@ def main():
 
   while True:
 
-      # sensorFlow1.updateValue()
-      # sensorFlow2.updateValue()
-      # sensorFlow3.updateValue()
-      # sensorFlow4.updateValue()
+      sensorFlow1.updateValue()
+      sensorFlow2.updateValue()
+      sensorFlow3.updateValue()
+      sensorFlow4.updateValue()
 
-      # root.label1['text']="{:^} MPa".format(sensorFlow1.pressure)
-      # root.label2['text']="{:^} MPa".format(sensorFlow2.pressure)
-      # root.label3['text']="{:^} MPa".format(sensorFlow3.pressure)
-      # root.label4['text']="{:^} MPa".format(sensorFlow4.pressure)
+      root.label1['text']="{:^} MPa".format(sensorFlow1.pressure)
+      root.label2['text']="{:^} MPa".format(sensorFlow2.pressure)
+      root.label3['text']="{:^} MPa".format(sensorFlow3.pressure)
+      root.label4['text']="{:^} MPa".format(sensorFlow4.pressure)
 
       #root.mainloop()
       root.update_idletasks()
